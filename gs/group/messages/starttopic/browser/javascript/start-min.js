3f47b1b82cd67727fa97cbe88c6461f747e4a4c9
Michael JasonSmith <mpj17@onlinegroups.net>
2013-05-24 09:27:46 -0700

    Following the changes to Add to Topic.

--- gs/group/messages/starttopic/browser/javascript/start-min.js
+++ gs/group/messages/starttopic/browser/javascript/start-min.js
@@ -1,5 +1,4 @@
 jQuery.noConflict();function GSGroupMessagesStartTopic(b){var c=null,e=null;function a(g,h){function f(i){e=i;
 h(e)}if(e==null){jQuery.post("topics.json",f)}else{h(e)}}function d(){c=jQuery(b);
-c.typeahead({source:a})}d()}jQuery(window).load(function(){var a=null,b=null;a=jQuery("#gs-group-messages-topic-add-privacy").html();
-b={animation:true,html:true,placement:"top",trigger:"click",content:a};jQuery("#gs-group-messages-topic-add-privacy-summary").popover(b);
-GSGroupMessagesStartTopic("#form\\.topic")});
+c.typeahead({source:a})}d()}jQuery(window).load(function(){GSGroupMessagesStartTopic("#form\\.topic")
+});
--- gs/group/messages/starttopic/browser/javascript/start.js
+++ gs/group/messages/starttopic/browser/javascript/start.js
@@ -25,11 +25,5 @@ function GSGroupMessagesStartTopic (topicInputId) {
 
 
 jQuery(window).load( function () {
-    var h = null, d = null;
-    h = jQuery('#gs-group-messages-topic-add-privacy').html();
-    d = {animation: true, html: true, placement: 'top', trigger: 'click', 
-         content: h};
-    jQuery('#gs-group-messages-topic-add-privacy-summary').popover(d);
-
     GSGroupMessagesStartTopic('#form\\.topic');
 });
