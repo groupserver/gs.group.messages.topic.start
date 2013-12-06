ca5b3825ab7d2386af94ca4ecae438553e2c08d6
Michael JasonSmith <mpj17@onlinegroups.net>
2013-12-06 12:16:11 -0800

    Not zip_safe, and adding to the egg deps.

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -29,11 +42,23 @@ setup(name='gs.group.messages.starttopic',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
     include_package_data=True,
-    zip_safe=True,
+    zip_safe=False,
     install_requires=[
         'setuptools',
+        'sqlalchemy',
+        'zope.app.form',
+        'zope.browserpage',
+        'zope.browserresource',
+        'zope.cachedescriptors',
+        'zope.component',
+        'zope.formlib',
+        'zope.schema',
+        'zope.tal',
+        'zope.tales',
+        'Zope2',
         'gs.content.js.bootstrap',  # For the JS
         'gs.content.layout',
+        'gs.database',
         'gs.group.base',
         'gs.group.member.canpost',
         'gs.group.messages.add.base',
