6e3746577b49b1e4f486a630c5c32a5651f20f7f
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-27 12:20:36 -0700

    Turning on strict mode.

--- gs/group/messages/starttopic/browser/javascript/start-min.js
+++ gs/group/messages/starttopic/browser/javascript/start-min.js
@@ -1,4 +1,4 @@
-jQuery.noConflict();function GSGroupMessagesStartTopic(b){var c=null,e=null;function a(g,h){function f(i){e=i;
-h(e)}if(e==null){jQuery.post("topics.json",f)}else{h(e)}}function d(){c=jQuery(b);
-c.typeahead({source:a})}d()}jQuery(window).load(function(){GSGroupMessagesStartTopic("#form\\.topic")
+"use strict";jQuery.noConflict();function GSGroupMessagesStartTopic(b){var c=null,e=null;
+function a(g,h){function f(i){e=i;h(e)}if(e==null){jQuery.post("topics.json",f)}else{h(e)
+}}function d(){c=jQuery(b);c.typeahead({source:a})}d()}jQuery(window).load(function(){GSGroupMessagesStartTopic("#form\\.topic")
 });
--- gs/group/messages/starttopic/browser/javascript/start.js
+++ gs/group/messages/starttopic/browser/javascript/start.js
@@ -1,8 +1,19 @@
+"use strict";
+// Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+// All Rights Reserved.
+//
+// This software is subject to the provisions of the Zope Public License,
+// Version 2.1 (ZPL). http://groupserver.org/downloads/license/
+//
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+// WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+// FITNESS FOR A PARTICULAR PURPOSE.
 jQuery.noConflict();
 
 function GSGroupMessagesStartTopic (topicInputId) {
     
-    var topicInput = null, topics = null;
+    var topicInput=null, topics=null;
 
     function topic_source(query, process) {
         function load(data) {
